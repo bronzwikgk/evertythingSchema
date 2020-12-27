97c8da5de89fd08de45dcd23c35021e8aa860403
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-27 21:44:18 +0530

    u

---
+++ json/roughWork.js
@@ -0,0 +1,49 @@
+
+let numbers = [1, 3, 5, 25]; let range = { min: 0, max: 15 };
+let inputNumer = 1;
+nonWordCharList = '/\\()"\':,.;<>~!@#$%^&*|+=[]{}`?-…';
+
+var inputA = ['html', 'richText', 'json', 'exe', 'css', 'javascript'];
+var inputB = ['html', 'richText', 'json', 'exe', 'css', 'javascript'];
+var inputStringA = "hello";
+var inputStringB = "hello";
+//output = operate.hasAllof(inputA, inputB);
+
+var inputElement = document.createElement("div")
+
+var inputObjA = {
+    "schema": {
+        "field": {
+            "input": "string",
+            "lable": "A field"
+        }
+    },
+    "form": [
+        {
+            "key": "field"
+        },
+        {
+            "button": "submit",
+            "lable": "Submit"
+        }
+    ]
+}
+
+
+var inputObjB = {
+    "schema": {
+        "field": {
+            "input": "string",
+            "lable": "A field"
+        }
+    },
+    "form": [
+        {
+            "key": "field"
+        },
+        {
+            "button": "submit",
+            "lable": "Submit"
+        }
+    ]
+}
--- scripts/temp code.js
+++ scripts/temp code.js
@@ -59,30 +59,7 @@ class operator {
 }
 
 
-function isEmpty(obj) {
-    for (var prop in obj) {
-        if (obj.hasOwnProperty(prop))
-            return false;
-    }
 
-    return JSON.stringify(obj) === JSON.stringify({});
-}
-
-
-//https://github.com/google/data-layer-helper
-//https://sites.google.com/site/scriptsexamples/custom-methods/2d-arrays-library
-
-
-//return true if all items are the same in two unordered Array need to add a return of mismatch values as option.
-function compareTwoArray_(arr1, arr2) {
-    arr1.sort();
-    arr2.sort();
-    if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) return false;
-    }
-    return true;
-}
 
 
 function checkHeaders(allHeaders, headersPassed, requiredHeaders) {
@@ -93,89 +70,11 @@ function checkHeaders(allHeaders, headersPassed, requiredHeaders) {
 
 }
 
-// Returns if a value is a string
-function isString(value) {
-    return typeof value === 'string' || value instanceof String;
-}
-
-// Returns if a value is really a number
-function isNumber(value) {
-    return typeof value === 'number' && isFinite(value);
-}
-
-
-
-
-// Returns if a value is an array
-function isArray(value) {
-    return value && typeof value === 'object' && value.constructor === Array;
-}
-
-// ES5 actually has a method for this (ie9+)
-//Array.isArray(value);
-
-// Returns if a value is a function
-function isFunction(value) {
-    return typeof value === 'function';
-}
-
-
-
-// // Returns if a value is an object
-// function isObject (value) {
-// return value && typeof value === 'object' && value.constructor === Object;
-// }
-
-
-
-// Returns if a value is null
-function isNull(value) {
-    return value === null;
-}
-
-// Returns if a value is undefined
-function isUndefined(value) {
-    return typeof value === 'undefined';
-}
-
-// Returns if a value is a boolean
-function isBoolean(value) {
-    return typeof value === 'boolean';
-}
-
-
-
-// Returns if a value is a regexp
-function isRegExp(value) {
-    return value && typeof value === 'object' && value.constructor === RegExp;
-}
-
-
-// Returns if value is an error object
-function isError(value) {
-    return value instanceof Error && typeof value.message !== 'undefined';
-}
-
-// Returns if value is a date object
-function isDate(value) {
-    return value instanceof Date;
-}
-
-
-//Returns if the value is a Prototyp
-function isPrototype(value) {
-    console.log(Object.getPrototypeOf(value) === prototype1);
-}
-
-
-// Returns if a Symbol
-function isSymbol(value) {
-    return typeof value === 'symbol';
-}
-
 
 //this function finds a key inside an object. Not recursivly.
 console.log("dataHelpers on")
+
+
 function find(entity, keyTofind) {
     console.log("finding", keyTofind, "in", entity);
     var result = Object.keys(entity).filter(function (key, index, self) {
