5d238bd0c3dc1452368f82e47ae44dd66edc55ad
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-01-05 14:28:46 +0530

    u

---
+++ scripts/actionEditorV6.js
@@ -0,0 +1,21 @@
+let editor = {
+    tagName : 'div',
+    id: 'actionEditor',
+    theme: 'light',
+    mimeMode:'text'
+}
+
+var toolbar = {
+    name: 'toolbar',
+    id: 'toolbar',
+    content: [
+        { "button": "new" },
+        { "button": "new2" },
+        { "button": "new3" }
+    ]
+}
+
+
+function init() { 
+
+}
