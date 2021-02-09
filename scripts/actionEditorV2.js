716021dbdc062cac0a8c2a653bcb116d27a3bffb
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-09 16:56:33 +0530

    u

--- scripts/actionEditorV2.js
+++ scripts/actionEditorV2.js
@@ -1,12 +1,15 @@
-// blockStatement: A block statement(or compound statement in other languages) is used to group zero or more statements.
-//The block is delimited by a pair of braces("curly brackets") and may optionally be labelled.
-// {
-//     StatementList
-// }
-
-// LabelIdentifier: {
-//     StatementList
-// }
+/**
+ * blockStatement: A block statement(or compound statement in other languages) is used to group zero or more statements.
+   The block is delimited by a pair of braces("curly brackets") and may optionally be labelled.
+   { StatementList }
+    LabelIdentifier: { StatementList }
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 
 var actionEditorConfig = {};
 
