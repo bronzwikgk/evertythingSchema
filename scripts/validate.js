abcd8dc47f4151cb1cbdce54c309f513c472b728
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-25 23:18:18 +0530

    u

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,29 +2,61 @@
 
 var Validator = function () { };
 
-Validator.prototype.validate = function (value, rules) {
-    var self = this;
-    return rules.every(function (rule) {
-        return self[rule](value);
-    });
-};
+class validator { 
+    validate(value, rules) { 
+        var self = this;
+        return rules.every(function (rule) {
+            return self[rule](value);
+        });
 
-Validator.prototype.isString = function (value) {
-    if (typeof value === 'string') {
-        return true;
     }
-    return false;
-};
 
-Validator.prototype.isNotEmpty = function (value) {
-    if (value !== '' && value !== null && typeof value !== 'undefined') {
-        return true;
+    isString (value) {
+        if (typeof value === 'string') {
+            return true;
+        }
+        return false;
     }
-    return false;
-};
+    isNotEmpty(value) {
+        if (value !== '' && value !== null && typeof value !== 'undefined') {
+            return true;
+        }
+        return false;
+    }
+    isInt = function (value) {
+        return Number.isInteger(value);
+    };
+    
+    // any other rule you want to add
+}
 
-Validator.prototype.isInt = function (value) {
-    return Number.isInteger(value);
-};
 
-// any other rule you want to add
+
+
+// Model.js
+class Model { 
+    constructor() { 
+        this.name = {
+        value: null,
+        validator: ['isString', 'isNotEmpty']
+    };
+    this.age = {
+        value: null,
+        validator: ['isInt']
+    }; 
+    }
+    set = function (value, key) {
+        if (this.validator.validate(value, key.validator)) {
+            key.value = value;
+            return true;
+        }
+        return false;
+    };
+    setName = function (name) {
+        this.set(name, this.name);
+    };
+    setAge = function (age) {
+        this.set(age, this.age);
+    };
+}
+
